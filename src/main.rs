a5a00bbfa833a5468c9d5812564fc8d5c4ebfb4f
Valentin Daviot <valentin.daviot@alterway.fr>
2019-08-13 11:22:40 +0200

    removed sh -c, now working on current env and shell, multicommand fixed
    Signed-off-by: Valentin Daviot <valentin.daviot@alterway.fr>

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,100 @@
 use std::io::{self, Write};
 use std::env;
-use std::process;
+use std::fs;
+use std::path::Path;
+use std::process::{{Command, exit}};
+use colored::*;
 
-fn make_prompt() -> String {
+fn make_prompt() -> colored::ColoredString {
     let username = "USERNAME";
     let mut prompt = String::new();
     match env::var(username) {
         Ok(value) => {
-            prompt.push_str(&(value + "@alterway"));
+            let value = value.green();
+            prompt.push_str(&(value));
         },
         Err(_) => {
             prompt.push_str("$");
         }
     }
-    prompt
+    prompt.bold()
+}
+
+fn check_command_availability(binary: &str) -> bool {
+    if let Ok(path) = env::var("PATH") {
+        for p in path.split(":") {
+            let p_str = format!("{}/{}", p, binary);
+            if fs::metadata(p_str).is_ok() {
+                return true;
+            }
+        }
+    }
+    false
 }
 
 
-fn execute_command(input: &str) {
-    let output = process::Command::new("sh").arg("-c").arg(input)
-                          .output()
-                          .expect("failed to execute command");
-    println!("\n{}", String::from_utf8_lossy(&output.stdout));
+fn execute_command(binary: &str, args: &str) {
+    if !check_command_availability(binary) {
+        println!("commnand not found in PATH");
+        return;
+    }
+    let mut child = Command::new(binary)
+                        .args(args.split_whitespace())
+                        .spawn()
+                        .expect("failed to run command");
+    let ecode = child.wait()
+                     .expect("failed to wait on child");
+
+    if !ecode.success() {
+        println!("child failed to execute command");
+    }
 }
 
 
-fn prepare_input(input: &str) -> Vec<&str> {
+fn split_multiple_commands(input: &str) -> Vec<&str> {
     input.split(';').collect()
 }
 
+
+fn change_directory(dir: &str) {
+    let path = Path::new(dir);
+    if let Err(e) = env::set_current_dir(&path) {
+        eprintln!("{}", e);
+    }
+}
+
+
+fn export_to_env(args: &str) {
+    let (envvar, value) = split_first(args, '=');
+    let value = &value[1..value.len()];
+    env::set_var(envvar, value);
+}
+
+
+fn exit_program() {
+    println!("Thanks for using seash!");
+    exit(0x0100);
+}
+
+
+fn execute_builtin(cmd: &str, args: &str) {
+    match cmd {
+        "cd"       => change_directory(args),
+        "export"   => export_to_env(args),
+        "exit" | _ => exit_program(),
+    }
+}
+
+fn split_first(cmd: &str, cmp: char) -> (&str, &str) {
+    let mut count = 0;
+    for c in cmd.trim().to_string().chars() {
+        if c == cmp {
+            return (&cmd[0..count].trim(), &cmd[count..cmd.len()].trim())
+        }
+        count = count + 1;
+    }
+    (cmd.trim(), "")
+}
 
 fn main() {
     let stdin = io::stdin();
@@ -43,17 +109,18 @@ fn main() {
             Ok(status) => status,
             Err(_) => continue,
         }
-        stdin.read_line(&mut input).expect("Failed to read user input");
-        let command_list = prepare_input(input.trim());
+        stdin.read_line(&mut input).unwrap();
+        if input.len() <= 1 {
+            continue;
+        }
+        let parsed_commands = split_multiple_commands(input.trim());
 
-        for command in command_list {
-            if command == "exit" {
-                println!("Thanks for using seash!");
-                process::exit(0x0100);
+        for command in parsed_commands {
+            let (binary, args) = split_first(command, ' ');
+            match binary {
+                "cd" | "exit" | "export" => execute_builtin(binary, args),
+                _                        => execute_command(binary, args),
             }
-
-            execute_command(command);
-
         }
     }
 }
